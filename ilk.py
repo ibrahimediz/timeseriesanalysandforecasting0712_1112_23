c71a6a431ed8099adcad8a5946b5d6099ff2b8e9
İbrahim EDİZ <edizdesign@gmail.com>
2023-12-07 08:06:04 +0000

    07122023_1

--- ilk.py
+++ ilk.py
@@ -1,12 +1,17 @@
 import os
+import shutil
 liste = os.listdir("Egzersizler/")
-filename = "01_ManipulateDataNote.ipynb"
+filename = "01_EgzersizManipulate.ipynb"
 for item in liste:
     adres = f"Egzersizler/{item}/{filename}"
+    target = f"Egzersizler/SoruCevap/{filename}"
+
     if not os.path.exists(adres):
-        # try:
-        #     print(adres[:-6])
-        #     os.remove(adres[:-6])
-        # except:
-        #     pass
-        open(adres,"wb")
+        shutil.copy(target,adres)
+    #     # try:
+    #     #     print(adres[:-6])
+    #     #     os.remove(adres[:-6])
+    #     # except:
+    #     #     pass
+    #     open(adres,"wb")
+
